243f65ccf96084374b739fb301de4bca797c9ce4
Yassin Abdelghany <yma.abdelghany@gmail.com>
2025-12-30 13:41:58 -0700

    Make possible to preprocess and merge datasets with one command

---
+++ scripts/preprocess_all.py
@@ -0,0 +1,18 @@
+import os
+
+scripts = [
+    "scripts/preprocess.py"
+    "scripts/preprocess_player_profiles.py",
+    "scripts/preprocess_player_performances.py",
+    "scripts/preprocess_market_value.py",
+    "scripts/preprocess_master_dataset.py",
+    "scripts/merge_datasets.py",
+]
+ 
+total_steps = len(scripts)
+
+for step, script in enumerate(scripts, start=1):
+    print(f"Step {step}/{total_steps}: Running {script} ...")
+    os.system(f"python {script}")
+
+print("Done preprocessing raw datasets. Next step: Make features dataset.")
